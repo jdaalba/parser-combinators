281e41a391cbe08dbe5ca068435a92cb49dedb47
jdaalba <jdaalba@gmail.com>
2022-03-15 22:27:11 +0100

    Or implemented

--- src/main/scala/com/jdaalba/Parsers.scala
+++ src/main/scala/com/jdaalba/Parsers.scala
@@ -14,16 +14,23 @@ object Parsers {
   def parseString(s: String): Parser[String] =
     input => if (input startsWith s) Some(s, input substring s.length) else None
 
+  // implicit constructors
+  implicit def token(s: String): Parser[String] = parseString(s)
+
   // syntax sugar
   implicit def asParserOps[A](p: Parser[A]): ParserOps[A] = ParserOps(p)
 
   case class ParserOps[A](p: Parser[A]) {
     def map[B](f: A => B): Parser[B] = ParserOps.map(p)(f)
+
+    def |(p2: Parser[A]): Parser[A] = ParserOps.or(p, p2)
   }
 
   // parser as Functor
   object ParserOps extends Functor[Parser] {
     override def map[A, B](fa: Parser[A])(f: A => B): Parser[B] =
       fa andThen (r => r map (t => (f(t._1), t._2)))
+
+    def or[A](p1: Parser[A], p2: Parser[A]): Parser[A] = s => p1(s).orElse(p2(s))
   }
 }
--- src/test/scala/com/jdaalba/ParsersTest.scala
+++ src/test/scala/com/jdaalba/ParsersTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 class ParsersTest extends AnyFlatSpec {
 
   import com.jdaalba.Parsers._
+  import com.jdaalba.Parsers.ParserOps._
 
   "parseChar" should "parse a character" in {
     assertResult(None)(parseChar('c')("abbra"))
@@ -15,5 +16,13 @@ class ParsersTest extends AnyFlatSpec {
     assertResult(Some(("", "abbra")))(parseString("")("abbra"))
     assertResult(None)(parseString("abbra")("cadabbra"))
     assertResult(Some(("abbra", " cadabbra")))(parseString("abbra")("abbra cadabbra"))
+  }
+
+  "or" should "choose, if exists, the non failed parser" in {
+    val p1: Parser[String] = "abbra"
+    val p2: Parser[String] = "cadabbra"
+    assertResult(None)((p1 | p2)("none"))
+    assertResult(Some("abbra", ""))((p1 | p2)("abbra"))
+    assertResult(Some("cadabbra", ""))((p1 | p2)("cadabbra"))
   }
 }
